06a38bb1ac24826a22a85131f233f12cc4dd8537
anand9773 <aaanand9773@gmail.com>
2023-11-29 20:11:52 +0530

    creating large varity of theme

---
+++ hamara_shop/lib/utils/theme/custom_themes/text_filed_theme.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class TTextFormFieldTheme {
+  TTextFormFieldTheme._();
+
+  static InputDecorationTheme lightInputDecorationTheme = InputDecorationTheme(
+    errorMaxLines: 3,
+    prefixIconColor: Colors.grey,
+    suffixIconColor: Colors.grey,
+    // constraints: const BoxConstraints.expand(height: 14,),
+    labelStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.black),
+    hintStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.black),
+    errorStyle: const TextStyle().copyWith(fontStyle: FontStyle.normal),
+    floatingLabelStyle:
+        const TextStyle().copyWith(color: Colors.black.withOpacity(0.8)),
+    border: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+
+     enabledBorder: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+
+     focusedBorder: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),    
+
+     errorBorder: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+    focusedErrorBorder: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+
+    
+    
+  );
+
+  static InputDecorationTheme darkInputDecorationTheme = InputDecorationTheme(
+    errorMaxLines: 2,
+    prefixIconColor: Colors.grey,
+    suffixIconColor: Colors.grey,
+    // constraints: const BoxConstraints.expand(height: 14,),
+    labelStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.white),
+    hintStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.white),
+    errorStyle: const TextStyle().copyWith(fontStyle: FontStyle.normal),
+    floatingLabelStyle:
+        const TextStyle().copyWith(color: Colors.white.withOpacity(0.8)),
+    border: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+
+     enabledBorder: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+
+     focusedBorder: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),    
+
+     errorBorder: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+    focusedErrorBorder: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+
+    
+    
+  );
+}
--- hamara_shop/lib/utils/theme/theme.dart
+++ hamara_shop/lib/utils/theme/theme.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hamara_shop/utils/theme/custom_themes/appbar_theme.dart';
+import 'package:hamara_shop/utils/theme/custom_themes/bottom_sheet_theme.dart';
+import 'package:hamara_shop/utils/theme/custom_themes/checkbox_theme.dart';
+import 'package:hamara_shop/utils/theme/custom_themes/chip_theme.dart';
+import 'package:hamara_shop/utils/theme/custom_themes/elevated_buttom_theme.dart';
+import 'package:hamara_shop/utils/theme/custom_themes/outline_buttom_theme.dart';
+import 'package:hamara_shop/utils/theme/custom_themes/text_filed_theme.dart';
 import 'package:hamara_shop/utils/theme/custom_themes/text_theme.dart';
 
 class TAppTheme {
@@ -9,10 +16,26 @@ class TAppTheme {
     fontFamily: 'Poppins',
     brightness: Brightness.light,
     primaryColor: Colors.blue,
+    chipTheme: TChipTheme.lightChipTheme,
+    checkboxTheme: TCheckboxTheme.lightCheckboxTheme,
     scaffoldBackgroundColor: Colors.black,
-    textTheme: TTextTheme.lightTextTheme,
-
-
+    bottomSheetTheme: TBottomSheetTheme.lightBottomSheetTheme,
+    elevatedButtonTheme:TElevatedButtonTheme.lightElevatedButtomTheme,
+    outlinedButtonTheme: TOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: TTextFormFieldTheme.lightInputDecorationTheme,
+    
   );
-  static ThemeData darkTheme = ThemeData();
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    chipTheme: TChipTheme.darkChipTheme,
+    checkboxTheme: TCheckboxTheme.darkCheckBoxTheme,
+    scaffoldBackgroundColor: Colors.black,
+    bottomSheetTheme: TBottomSheetTheme.darkBottomSheetTheme,
+    elevatedButtonTheme:TElevatedButtonTheme.darkElevatedButtomTheme,
+    outlinedButtonTheme: TOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: TTextFormFieldTheme.darkInputDecorationTheme,
+  );
 }
