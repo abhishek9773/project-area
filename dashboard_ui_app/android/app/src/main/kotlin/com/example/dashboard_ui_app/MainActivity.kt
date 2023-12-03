8cde737f0973f40a39d1d017790b7a7e3cad6d3c
anand9773 <aaanand9773@gmail.com>
2023-12-04 00:47:35 +0530

    all other project are shifted to another place

--- dashboard_ui_app/android/app/src/main/kotlin/com/example/dashboard_ui_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dashboard_ui_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- dommy_project/android/app/src/main/kotlin/com/example/dommy_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dommy_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
